24e3a4c1a7510c08db45521c65a2da3423ab9b46
slenkis <zaharkin629@gmail.com>
2018-11-12 19:23:08 +0300

    для володи

--- src/logic/InputCreator.java
+++ src/logic/InputCreator.java
@@ -1,5 +1,7 @@
 package logic;
 
+import logic.commands.AddCommand;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,12 +11,15 @@ public class InputCreator {
     private List<String> preList = new ArrayList<>();
 
     public void addWhile() {
+        AddCommand adC = new AddCommand();
+        List<String> innerList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             System.out.println("Что хотите добавить?");
-            String inner = sc.nextLine().trim();
+            inner.add(sc.nextLine().trim());
             if (!inner.equals("")) {
-
+                innerList.add()
+                adC.satisfy(inner, preList);
             } else {
                 break;
             }
--- src/logic/commands/AddCommand.java
+++ src/logic/commands/AddCommand.java
@@ -10,7 +10,7 @@ public class AddCommand implements Command {
     @Override
     public void satisfy(String[] args, List<String> arr) {
         for (String arg : args) {
-
+            arr.add()
         }
     }
 
